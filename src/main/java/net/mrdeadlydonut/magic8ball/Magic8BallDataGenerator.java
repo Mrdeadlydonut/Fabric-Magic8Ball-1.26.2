530730bb023b8655e311c1ee45ecda51bb9d004e
Mrdeadlydonut <rayala875@gmail.com>
2026-05-24 17:35:25 -0500

    Making the 8 Ball Item without Textures

---
+++ src/main/java/net/mrdeadlydonut/magic8ball/Magic8BallDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mrdeadlydonut.magic8ball;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Magic8BallDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/mrdeadlydonut/magic8ball/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.mrdeadlydonut.magic8ball.item;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.mrdeadlydonut.magic8ball.Magic8Ball;
+
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item Magic_8_Ball = registerItem("Magic 8 Ball", Item::new);
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(Magic8Ball.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Magic8Ball.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        Magic8Ball.LOGGER.info("Registering Mod Items for " + Magic8Ball.MOD_ID);
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(output -> {
+            output.accept(Magic_8_Ball);
+        });
+    }
+}
